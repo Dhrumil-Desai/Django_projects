16e3443b4a5a476cd14d794a6a8016976dcb49d7
Dhrumil-Desai <142579607+Dhrumil-Desai@users.noreply.github.com>
2026-07-13 11:46:25 +0530

    Delete HospitalProject directory

--- HospitalProject/hospitalapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-@admin.register(Patient)
-class Patient(admin.ModelAdmin):
-    list_display = ['name','age','gender','phone','address','blood_group','doctor_name','is_admitted','timestamp']
-# Register your models here.
