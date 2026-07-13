74f73497c0b04693292a16e0c65ee9732335cc4e
Dhrumil-Desai <142579607+Dhrumil-Desai@users.noreply.github.com>
2026-07-13 11:46:36 +0530

    Delete LibraryProject directory

--- LibraryProject/libraryapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Book
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ["name","author","description","price","pages_count","sold_count","is_featured","is_trending","timestamp"]
-# Register your models here.
